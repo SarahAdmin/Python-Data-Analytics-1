6daf0759695dab29f0d27cd3dbd9efd2cae7af78
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-06 21:56:32 +0000

    Create advanced-containers.py

---
+++ advanced-containers.py
@@ -0,0 +1,7 @@
+students = {['Lee','Cox','July 10'],['Matt','Bond','January 1'], ['Hope','Dawson','April 6'],['Tara',
+  'Smith','June 11']}  
+  students{0}[0]
+  students{0}[1] 
+  students{1}[0] 
+  students{1}[1]
+
