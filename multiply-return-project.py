2e0d210c659074721ab0215562592deb4b3ee3b2
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-09 14:48:36 +0000

    Update multiply-return-project.py

--- multiply-return-project.py
+++ multiply-return-project.py
@@ -1,6 +1,6 @@
 def MultiplyNumbers(num): 
   return num * 2  
-print(MutltiplyNumber(1))
+print(MultiplyNumbers(1))
 print(MultiplyNumbers(2))
 print(MultiplyNumbers(3)) 
 print(MultiplyNumbers(4))
