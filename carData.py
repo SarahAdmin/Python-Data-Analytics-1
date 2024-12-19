b96653abb878b2ddd3d7911278a0b2d7368a77b6
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-19 12:54:23 +0000

    Create carData.py

---
+++ carData.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+myData2 = { 
+'Make': ["Toyata","Ford","Volkswagen"],
+'Model':["Yaris,","Fiesta","T-Cross"], 
+'Colour':["Red","Blue","Black"], 
+'No_of_Seats':[5,5,5], 
+'Year':[2019,2024,2020]
+}
+myVar1 = pd.DataFrame(myData2) 
+print(myVar1)
