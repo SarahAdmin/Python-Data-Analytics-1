12b5bc282b061828b65d50e5e580aac1f2ad44c1
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-19 12:40:43 +0000

    Create myDataOne.py

---
+++ myDataOne.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+myData = { 
+'student_id': [285,100,495]
+'name': ["Lee Cox",'"Matt Bond",'"Lemon Jelly"]
+'course':["Computing","Finance","Accounting"]
+
+}
+myVar = pd.DataFrame(myData) 
+print(myVar)
