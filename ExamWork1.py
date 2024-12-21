7c56666c576c5014b6837313b328dc0a8fe8bc67
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-21 19:52:51 +0000

    Update ExamWork1.py

--- ExamWork1.py
+++ ExamWork1.py
@@ -3,9 +3,13 @@ import pandas as pd
 StudentData1 = {"Name":['Lee Cox','Matt Bond','Lemon Jelly','Marcellina Adams','Jill Gates','Joe Malone'], 
                 "Qualification":['FS','FS','FS','FS','FS','FS'],
                 "Level":['L1','L1','E3','E3','L1','E3'],
-                "Subject":['English','English','English','English','English','English']
+                "Subject":['English','Maths','English','Maths','English','Maths'],
                 "Passed":[True,True,True,False,False,True]
               }
+StudentData2 = { "Retake?": [False,False,False,True,True]
+             } 
 
 Variables2 = pd.DataFrame(StudentData1) 
-print(Variables2)
+df3 = pd.DataFrame(StudentData2) 
+Results  = Variables2.join(df3)
+print(Results)
