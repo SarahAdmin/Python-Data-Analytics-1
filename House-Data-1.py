a66957896e760102e1a721f2d6c536ead4407367
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-28 20:56:45 +0000

    Update House-Data-1.py

--- House-Data-1.py
+++ House-Data-1.py
@@ -4,5 +4,5 @@ print(hdf)
 total_new_price = hdf['New_Build_Average_Price'].sum() 
 print(f"The total price for new building is: {total_new_price}")  
 total_e_price = hdf['Existing_Property_Average_Price'].sum() 
-print(f"The total price for existing building is {average_e_price}") 
+print(f"The total price for existing building is {total_e_price}") 
 
