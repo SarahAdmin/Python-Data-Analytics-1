1adcd69d72874fe2481868086ffa2411d80b38c8
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-28 19:19:11 +0000

    Update House-Data-1.py

--- House-Data-1.py
+++ House-Data-1.py
@@ -1,9 +1,8 @@
 import pandas as pd 
-hdf = pd.read_csv('New-and-Old-2024-09.csv') 
-data2 = hdf.dropna()  
-print(data2.to_string())
-average_new_price = data2['New_Build_Average_Price'].mean() 
-print(f"The average price for new building is: {average_new_price}")  
-average_e_price = data2['Existing_Property_Average_Price'].mean() 
-print(f"The average price for existing building is {average_e_price}") 
+hdf = pd.read_csv('New-and-Old-2024-09.csv')   
+print(hdf)
+total_new_price = hdf['New_Build_Average_Price'].sum() 
+print(f"The total price for new building is: {total_new_price}")  
+total_e_price = hdf['Existing_Property_Average_Price'].sum() 
+print(f"The total price for existing building is {average_e_price}") 
 
