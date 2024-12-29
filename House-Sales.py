ebff45422f286d481482e390c94265bfa9ee7fec
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-29 16:55:17 +0000

    Update House-Sales.py

--- House-Sales.py
+++ House-Sales.py
@@ -1,3 +1,6 @@
 import pandas as pd 
 salesData = pd.read_csv("Sales-2024-10.csv") 
 print(salesData.to_string())
+salesAverage = salesData["Sales_Volume"].mean() 
+print(f'The average sales volume is {salesAverage}') 
+
