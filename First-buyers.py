79bea0573e3356c21ec4ded684305874ef953e9d
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-30 14:14:12 +0000

    Update First-buyers.py

--- First-buyers.py
+++ First-buyers.py
@@ -1,4 +1,8 @@
 import pandas as pd 
 hdf = pd.read_csv('First-Time-Buyer-Former-Owner-Occupied-2024-10.csv') 
-data1 = hdf.dropna() 
-print(data1)
+total_first_time_price = hdf["First_Time_Buyer_Average_Price"].sum() 
+total_former_price = hdf["Former_Owner_Occupier_Average_Price"].sum() 
+print(f"The total price of First Time Buyer Price is {total_first_time_price}.")
+print(f"The total price of Former Buyer Price is {total_former_price}.")
+
+
