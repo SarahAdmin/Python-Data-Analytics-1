6db2378774afcd173ee0a5007ebd15c01df5a584
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-06 12:56:45 +0000

    Create PizzaOrder.py

---
+++ PizzaOrder.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+studentData = {"Name":['Lee Cox','Matt Bond','Joe Malone','Jenny Banks','Marcellina Adams','Paige Quinn','John Baxter'],
+              "Topping":['Meat Feast','Pepperoni','Meat Feast','BBQ Chicken','Pepperoni','Cheese and Tomato','Meat Feast'],
+              "Size":[10,8,8,10,8,8,10], 
+              "Price":[7.50,5.00,5.00,7,50,5.00,5.00,7,50]}
+additionalData = {"Allergies":[False,False,False,False,False,False,False]}
+
+var1 = pd.DataFrame(studentData) 
+var2 = pd.DataFrame(additionalData) 
+odf = var1.join(var2) 
+print(odf.to_string())
