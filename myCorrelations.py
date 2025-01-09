ad0d7ee6f78ea9317e481f9ff43e40368720f08b
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-09 12:55:23 +0000

    Update myCorrelations.py

--- myCorrelations.py
+++ myCorrelations.py
@@ -1,4 +1,5 @@
 import pandas as pd 
 
 latedf = pd.read_csv('CSV_HMCTS_management_information_Nov23toNov24.csv')
-print(latedf.corr())
+newlatedf = latedf.drop(columns=["Month"])
+print(newlatedf.corr())
