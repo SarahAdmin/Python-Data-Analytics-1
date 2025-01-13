bb9dca90a0c68f06813405885716e0a896d89969
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-13 16:34:11 +0000

    Update driving_test_correlations.py

--- driving_test_correlations.py
+++ driving_test_correlations.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 drivedf = pd.read_csv('drt121e-car-driving-tests-with-zero-faults.csv')
 drivedf.drop(columns['Year','Car driving test pass rate','Percentage of car driving test conducted with zero faults','Percentage of car driving test passes with zero faults'])
-print(drivedf.corr())
+print(drivedf.corr(method='pearson'))
