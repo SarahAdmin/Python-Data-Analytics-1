3e730b5bf5083a1cbd5e4ed0a2fbeb75ff8231bd
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-13 16:50:51 +0000

    Update HouseSales-Correlations.py

--- HouseSales-Correlations.py
+++ HouseSales-Correlations.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 sdf = pd.read_csv('New-and-Old-2024-09.csv')
 NewSDF = sdf.drop(columns=['Date','Region_Name','Area_Code']) 
-print(NewSDF.corr())
+print(NewSDF.corr(method='pearson', min_periods= 1, numeric_only=False))
