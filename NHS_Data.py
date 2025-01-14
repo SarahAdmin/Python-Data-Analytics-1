5fb5714042d3eb2f4c002d0ae2006f71cacb0b60
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-14 13:46:32 +0000

    Update NHS_Data.py

--- NHS_Data.py
+++ NHS_Data.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 hdf = pd.read_csv('03 - June.csv', encoding='latin-1')
-print(hdf.to_string()) 
+print(hdf.head(101)) 
+hdf['Amount'] = pd.to_numeric(hdf['Amount'].str.replace('[^0-9.-]', '', regex=True))
 total_amount = hdf["Amount"].sum() 
 average_amount = hdf["Amount"].mean() 
 print(f'The Total Amount for NHS is {total_amount}.') 
