44f55a3d399778efeea455b3ba251606a4939305
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-14 13:48:22 +0000

    Update driving_tests_1.py

--- driving_tests_1.py
+++ driving_tests_1.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 driveDF = pd.read_csv('drt121e-car-driving-tests-with-zero-faults.csv')
 print(driveDF.to_string()); 
+driveDF['Total car driving tests conducted'] = pd.to_numeric(hdf['Total car driving tests conducted'].str.replace('[^0-9.-]', '', regex=True))
+driveDF['Total car driving tests passed'] = pd.to_numeric(hdf['Total car driving tests passed'].str.replace('[^0-9.-]', '', regex=True))
+driveDF['Total car driving tests passed with zero faults'] = pd.to_numeric(hdf['Total car driving tests passed with zero faults'].str.replace('[^0-9.-]', '', regex=True))
 average_score_1 = driveDF["Total car driving tests conducted"].mean() 
 average_score_2 = driveDF["Total car driving tests passed"].mean()
 average_score_3 = driveDF["Total car driving tests passed with zero faults"].mean()
