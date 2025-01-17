7b2b67e9c34bb7f3678618f3ef5e5052ed5a756a
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-17 13:45:41 +0000

    Update Company_in_NHS.py

--- Company_in_NHS.py
+++ Company_in_NHS.py
@@ -1,4 +1,4 @@
 import pandas as pd 
 
 df1 = pd.read_csv('03 - June.csv', encoding='latin-1')
-print(df1.where(df1['Supplier'] == 'IBM UK LTD').dropna())
+print(df1.where(df1['Supplier'] == 'IBM UK LTD').dropna(axis=0, inplace=True))
