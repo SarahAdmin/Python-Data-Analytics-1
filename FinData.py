048796f7db5ed97f74ca2213dfb0c2f58d7e43c8
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-17 13:45:14 +0000

    Update FinData.py

--- FinData.py
+++ FinData.py
@@ -1,5 +1,5 @@
 import pandas as pd 
 
-df1 = pd.read_csv('finance_data1.csv')
-new_df = df1.dropna() 
+df1 = pd.read_csv('finance_data1.csv',header=0, sep=',')
+new_df = df1.dropna(axis=0, inplace=True) 
 print(new_df.to_string())
