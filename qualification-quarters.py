fcd616d9c8d2f204395dad39e854d8b4648c94d7
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-18 18:47:40 +0000

    Create qualification-quarters.py

---
+++ qualification-quarters.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+df1 = pd.read_csv('regulated-qualifications-dataset-Jan-2012-to-present-England.csv', encoding='latin-1')
+print(df1.head(51)) 
+
+average_quarter_1_2023 = df1[2023.1].mean() 
+average_quarter_2_2023 = df1[2023.2].mean() 
+average_quarter_3_2023 = df1[2023.3].mean() 
+average_quarter_4_2023 = df1[2023.4].mean() 
+average_quarter_1_2024 = df1[2024.1].mean() 
+average_quarter_2_2024 = df1[2024.2].mean() 
+average_quarter_3_2024 = df1[2024.3].mean() 
+
+print(f"The average result of first quarter of 2023 is {average_quarter_1_2023}.")
+print(f"The average result of second quarter of 2023 is {average_quarter_2_2023}.")
+print(f"The average result of third quarter of 2023 is {average_quarter_3_2023}.")
+print(f"The average result of fourth quarter of 2023 is {average_quarter_4_2023}.")
+print(f"The average result of first quarter of 2024 is {average_quarter_1_2024}.")
+print(f"The average result of second quarter of 2024 is {average_quarter_2_2024}.")
+print(f"The average result of third quarter of 2024 is {average_quarter_3_2024}.")
+v
+
+
