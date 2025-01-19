7b11422eeee23c2f0baa51a512f1662d5930ebe9
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-01-19 19:14:25 +0000

    Create 2-D_string_array.py

---
+++ 2-D_string_array.py
@@ -0,0 +1,5 @@
+import numpy as np 
+myArr = np.array([['Cola','Milkshake','Juice'],['Lemonade','Tea','Coffee']])
+print(myArr)
+                                              
+                                                
